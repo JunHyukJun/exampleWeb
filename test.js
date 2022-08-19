cbde74415c156570120811260a5d692b826aaf06
JunHyukJun <rnjs79153@gmail.com>
2022-08-19 15:46:22 +0900

    test

---
+++ test.js
@@ -0,0 +1,14 @@
+
+// const fs = require('fs');
+// var data = fs.readFileSync('c:/Temp/read.txt','utf-8');
+// console.log(data);
+// console.log('Done');
+
+
+// const fs = require('fs');
+// fs.readFile('c:/ModernWeb/workspace/exampleWeb/read.txt','utf-8',(err,data)=>{
+//     if (err) return console.log('err', err);
+ 
+//     console.log('data:', data);
+// })
+// console.log('Done');
